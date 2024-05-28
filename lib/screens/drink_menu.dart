2d9b2821d6669a45c79e844ed1aa5d9fe379e737
Vilar95 <e.vilar95@gmail.com>
2024-05-28 16:37:31 -0300

    cardapio

---
+++ lib/screens/drink_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/drink_item.dart';
+
+class DrinkMenu extends StatelessWidget {
+  DrinkMenu({super.key});
+
+  final List items = drinks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                "Bebidas",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+              ),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return DrinkItem(
+                  imageURI: items[index]['image'],
+                  itemTitle: items[index]['name'],
+                  itemPrice: items[index]['price'],
+                );
+              },
+              childCount: items.length,
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: MediaQuery.of(context).orientation == Orientation.landscape ? 3 : 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: MediaQuery.of(context).orientation == Orientation.landscape ? 1.2 : 0.81,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/food_menu.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
+
+class FoodMenu extends StatelessWidget {
+  FoodMenu({super.key});
+
+  final List items = comidas;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 1.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return FoodItem(
+                  itemTitle: items[index]["name"],
+                  itemPrice: items[index]["price"],
+                  imageURI: items[index]["image"],
+                );
+              },
+              childCount: items.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
